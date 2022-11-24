8858ee08ef206837e9b1bdcaca377ac3d82ca2b6
rohit@git <sharma.rohit9031@gmail.com>
2022-11-24 20:24:54 +0530

    monorepo mfe

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,17 @@
+import {mount} from 'marketing/MarketingApp';
+import React,{ useEffect,useRef } from 'react';
+
+function MarketingApp() {
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    },[])
+
+  return (
+    <div ref={ref}></div>
+  )
+}
+
+export default MarketingApp
