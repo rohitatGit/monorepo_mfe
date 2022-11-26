cb9c2f4710a82c988c9cc7b08304bfa15a33bac2
rohit@git <sharma.rohit9031@gmail.com>
2022-11-26 17:41:14 +0530

    added new changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {BrowserRouter }  from 'react-router-dom';
+import { StylesProvider,createGenerateClassNames } from '@material-ui/core/styles';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 //console.log(mount);
+const generateClassName = createGenerateClassName({
+    productionPrefix : 'co'
+  });
+
 
 export default () => {
   return <>
   <BrowserRouter>
+  <StylesProvider>
   <Header/>
   <MarketingApp/>
+  </StylesProvider>
   </BrowserRouter>
   </> ;
 };
