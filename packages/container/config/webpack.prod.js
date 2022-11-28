b0af2749a2096884fb531dcce937612e13cfa7db
rohit@git <sharma.rohit9031@gmail.com>
2022-11-28 16:34:05 +0530

    added auth remote in container webpack prod

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name : "container",
             remotes : {
-              'marketing' : `marketing@${domain}/marketing/latest/remoteEntry.js`
+              'marketing' : `marketing@${domain}/marketing/latest/remoteEntry.js`,
               'auth' : `auth@${domain}/auth/latest/remoteEntry.js`
             },
             shared: package.dependencies
